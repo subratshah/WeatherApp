650e1d887075a0a8a12632515ad06f9c905358bb
Subrat Shah <subrat.s94@gmail.com>
2019-07-01 17:46:52 +0530

    model class

---
+++ app/src/main/java/com/example/weatherapp/Model.java
@@ -0,0 +1,281 @@
+package com.example.weatherapp;
+
+public class Model {
+    private Coord coord;
+    Weather weather;
+    String baseStations;
+    Main main;
+    int visibility;
+    Wind wind;
+    Clouds clouds;
+    String dt;
+    Sys sys;
+    String timezone;
+    String id;
+    String Chennai;
+    String name;
+    int cod;
+
+    public String getBaseStations() {
+        return baseStations;
+    }
+
+    public void setBaseStations(String baseStations) {
+        this.baseStations = baseStations;
+    }
+
+    public int getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(int visibility) {
+        this.visibility = visibility;
+    }
+
+    public String getDt() {
+        return dt;
+    }
+
+    public void setDt(String dt) {
+        this.dt = dt;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getChennai() {
+        return Chennai;
+    }
+
+    public void setChennai(String chennai) {
+        Chennai = chennai;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public static class Coord {
+        private double lon;
+        private double lat;
+
+        public double getLon() {
+            return lon;
+        }
+
+        public void setLon(double lon) {
+            this.lon = lon;
+        }
+
+        public double getLat() {
+            return lat;
+        }
+
+        public void setLat(double lat) {
+            this.lat = lat;
+        }
+
+    }
+
+    public static class Weather {
+        private int id;
+        private String main;
+        private String description;
+        private String icon;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getMain() {
+            return main;
+        }
+
+        public void setMain(String main) {
+            this.main = main;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getIcon() {
+            return icon;
+        }
+
+        public void setIcon(String icon) {
+            this.icon = icon;
+        }
+    }
+
+    public static class Main {
+        private double temp;
+        private double pressure;
+        private double humidity;
+        private double temp_min;
+        private double temp_max;
+
+        public double getTemp() {
+            return temp;
+        }
+
+        public void setTemp(double temp) {
+            this.temp = temp;
+        }
+
+        public double getPressure() {
+            return pressure;
+        }
+
+        public void setPressure(double pressure) {
+            this.pressure = pressure;
+        }
+
+        public double getHumidity() {
+            return humidity;
+        }
+
+        public void setHumidity(double humidity) {
+            this.humidity = humidity;
+        }
+
+        public double getTemp_min() {
+            return temp_min;
+        }
+
+        public void setTemp_min(double temp_min) {
+            this.temp_min = temp_min;
+        }
+
+        public double getTemp_max() {
+            return temp_max;
+        }
+
+        public void setTemp_max(double temp_max) {
+            this.temp_max = temp_max;
+        }
+    }
+
+    public static class Wind {
+        private double speed;
+        private double deg;
+
+        public double getSpeed() {
+            return speed;
+        }
+
+        public void setSpeed(double speed) {
+            this.speed = speed;
+        }
+
+        public double getDeg() {
+            return deg;
+        }
+
+        public void setDeg(double deg) {
+            this.deg = deg;
+        }
+    }
+
+    public static class Clouds {
+        private int all;
+
+        public int getAll() {
+            return all;
+        }
+
+        public void setAll(int all) {
+            this.all = all;
+        }
+    }
+
+    private class Sys {
+        private int type;
+        private int id;
+        private String message;
+        private String country;
+        private String sunrise;
+        private String sunset;
+
+        public int getType() {
+            return type;
+        }
+
+        public void setType(int type) {
+            this.type = type;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public String getSunrise() {
+            return sunrise;
+        }
+
+        public void setSunrise(String sunrise) {
+            this.sunrise = sunrise;
+        }
+
+        public String getSunset() {
+            return sunset;
+        }
+
+        public void setSunset(String sunset) {
+            this.sunset = sunset;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/weatherapp/WeaherViewModel.java
@@ -0,0 +1,5 @@
+package com.example.weatherapp;
+
+public class WeaherViewModel {
+    private Model model;
+}
