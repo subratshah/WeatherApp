49a62aaa56b71cb4bdda9eaca3afd60d855bbd89
Subrat Shah <subrat.s94@gmail.com>
2019-07-01 18:24:29 +0530

    api integration

--- app/src/main/java/com/example/weatherapp/Api.java
+++ app/src/main/java/com/example/weatherapp/Api.java
@@ -1,6 +1,11 @@
 package com.example.weatherapp;
 
-public class Api {
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
 
-    @GET("")
+public interface Api {
+
+    @GET("?q={city},{country}")
+    Call<Model> getWeather(@Path("city") String city, @Path("country") String country);
 }
